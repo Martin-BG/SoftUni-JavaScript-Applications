71b2c53616780e6c8acf702bce66461adee80761
MartinBG <MartinBG@abv.bg>
2018-12-09 03:04:37 +0200

    implement ad edit

--- 06. Single Page Application/Exercises/Prodavachnik/scripts/publication.js
+++ 06. Single Page Application/Exercises/Prodavachnik/scripts/publication.js
@@ -26,17 +26,17 @@ const publication = (() => {
     const imageUrl = $form.find('input[name="imageUrl"]').val().trim();
     const publisher = user.name();
 
-    if (title.length === 0) {
-      notifications.error('Title can\'t be empty');
-      return;
-    }
+    // if (title.length === 0) {
+    //   notifications.error('Title can\'t be empty');
+    //   return;
+    // }
+    //
+    // if (price.length === 0) {
+    //   notifications.error('Price can\'t be empty');
+    //   return;
+    // }
 
-    if (price.length === 0) {
-      notifications.error('Price can\'t be empty');
-      return;
-    }
-
-    const newAdd = {
+    const newAd = {
       title,
       description,
       price,
@@ -45,7 +45,7 @@ const publication = (() => {
     };
 
     try {
-      await requester.post('appdata', 'adverts', '', newAdd);
+      await requester.post('appdata', 'adverts', '', newAd);
       view.show('viewAds');
       notifications.info('Ad created!');
       $form[0].reset(); // === $form.trigger('reset');
@@ -54,31 +54,61 @@ const publication = (() => {
     }
   };
 
-// Delete an add
-  const remove = async () => {
+  const remove = async (event) => {
     const $publication = $($(event.currentTarget).parent());
     const id = $publication.attr('data-id');
-    requester.remove('appdata', 'adverts' + '/' + id, '');
+    await requester.remove('appdata', `adverts/${id}`, '');
     $publication.parent().remove();
   };
 
-// Edit an add
-  const edit = async (id, publisher, date) => {
-    // TODO
-    console.log(event.currentTarget);
+  const edit = async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const $form = $('#formEditAd');
+    const id = $form.find('input[name="id"]').val();
+    const title = $form.find('input[name="title"]').val().trim();
+    const description = $form.find('textarea[name="description"]').val().trim();
+    const price = $form.find('input[name="price"]').val().trim();
+    const imageUrl = $form.find('input[name="imageUrl"]').val().trim();
+
+    const ad = {
+      title,
+      description,
+      price,
+      imageUrl
+    };
+
+    try {
+      await requester.update('appdata', `adverts/${id}`, '', ad);
+      view.show('viewAds');
+      notifications.info('Ad edited!');
+      $form[0].reset(); // === $form.trigger('reset');
+    } catch (error) {
+      notifications.handleError(error);
+    }
   };
 
-// Open edit add view
-  const openEditAdd = async () => {
-    // TODO
-    console.log(event.currentTarget);
+  const openEditAdd = async (event) => {
+    const $publication = $($(event.currentTarget).parent());
+    const id = $publication.attr('data-id');
+
+    const ad = await requester.get('appdata', `adverts/${id}`, '');
+
+    const $form = $('#formEditAd');
+    $form.find('input[name="id"]').val(id);
+    $form.find('input[name="publisher"]').val(user.name());
+    $form.find('input[name="title"]').val(ad.title);
+    $form.find('textarea[name="description"]').val(ad.description);
+    $form.find('input[name="price"]').val(ad.price);
+    $form.find('input[name="imageUrl"]').val(ad.imageUrl);
+    $form.on('submit', edit);
+
+    view.show('viewEditAd');
   };
 
   return {
     all,
-    create,
-    remove,
-    edit,
-    openEditAdd,
+    create
   };
 })();
